a1fc6ac5d828845a38f17e13123b3fede545e5f2
prymy <96066363+prymy@users.noreply.github.com>
2021-12-13 22:04:50 +0700

    Delete api directory

--- api/ruapi.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	let params = (function(){var vars={};var parts=window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(m,key,value){vars[key]=value;});return vars;})();
-	let fafifu = /bot|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(navigator.userAgent);
-	if(params.id) {
-		if(fafifu) location.replace('https://aliexpress.ru/item/'+params.id+'.html');
-		else location.href = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key=_ALiWwG&dl_target_url=https://aliexpress.ru/item/'+params.id+'.html';
-	}
-	if(params.gotcha) { document.addEventListener('DOMContentLoaded', function(){ document.title = 'Gotcha'; }); }
-})();
